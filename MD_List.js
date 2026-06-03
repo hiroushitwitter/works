54dc8f420b4efdb8d09143132db028b9305cc33e
hiroushitwitter <105724089+hiroushitwitter@users.noreply.github.com>
2026-06-03 10:34:42 +0900

    Update MD_List.js

--- MD_List.js
+++ MD_List.js
@@ -442,7 +442,7 @@ function test_func(){
 
 console.log(Object.keys(test_out_group2[target_SAKUBAN]).filter(a => {
 		console.log(a);
-		a.startsWith(target_KIBAN.trim());
+		return a.startsWith(target_KIBAN.trim());
 	}));
 	let result_MFG = test_out_group2[target_SAKUBAN][target_KIBAN.trim()].map(a => {
 		const MFG_Days1 = new Date((a[0] - 25569) * 86400000).toISOString().split('T')[0].replaceAll("-","/");
